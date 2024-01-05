0f4a5ae9ec2dcab7be705e4299d28fb8b61cc798
martelligiovi <martelligiovi@gmail.com>
2024-01-05 20:53:12 -0300

    log p1

---
+++ src/main/java/com/system/blog/servicio/UserServicio.java
@@ -0,0 +1,15 @@
+package com.system.blog.servicio;
+
+import com.system.blog.DTO.UserDTO;
+
+import java.util.List;
+
+public interface UserServicio {
+
+    public UserDTO crearUser(UserDTO userDTO);
+    public UserDTO obtenerUserPorId(Long id);
+    public void eliminarUser(Long id);
+    public List<UserDTO> obtenerUsers();
+
+
+}
---
+++ src/main/java/com/system/blog/servicio/UserServicioImpl.java
@@ -0,0 +1,31 @@
+package com.system.blog.servicio;
+
+import com.system.blog.DTO.UserDTO;
+
+import java.util.List;
+
+public class UserServicioImpl extends UserServicio{
+    @Override
+    public UserDTO crearUser(UserDTO userDTO) {
+
+
+
+        return null;
+
+    }
+
+    @Override
+    public UserDTO obtenerUserPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void eliminarUser(Long id) {
+
+    }
+
+    @Override
+    public List<UserDTO> obtenerUsers() {
+        return null;
+    }
+}
